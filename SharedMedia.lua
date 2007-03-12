d3cb45f221b344599960db7e198c52d08837501a
elkano <elkano@1a92d559-679e-4034-997b-ece41c1f7aad>
2007-03-12 17:14:34 +0000

    SharedMedia - initial commit - adds media to SharedMediaLib's storage
    git-svn-id: https://repos.wowace.com/wow/sharedmedia/tmp/trunk/SharedMedia@22 1a92d559-679e-4034-997b-ece41c1f7aad

---
+++ SharedMedia.lua
@@ -0,0 +1,27 @@
+local SML = AceLibrary("SharedMedia-1.0")
+
+SharedMedia = AceLibrary("AceAddon-2.0"):new()
+
+function SharedMedia:OnInitialize()
+
+-- -----
+--   FONT
+-- -----
+	-- SML:Register("font", "", "")
+
+
+-- -----
+--   STATUSBAR
+-- -----
+	SML:Register("statusbar", "BantoBar",	"Interface\\Addons\\SharedMedia\\statusbar\\BantoBar")
+	SML:Register("statusbar", "Charcoal",	"Interface\\Addons\\SharedMedia\\statusbar\\Charcoal")
+	SML:Register("statusbar", "Cilo",		"Interface\\Addons\\SharedMedia\\statusbar\\Cilo")
+	SML:Register("statusbar", "Glaze",		"Interface\\Addons\\SharedMedia\\statusbar\\Glaze")
+	SML:Register("statusbar", "Gloss",		"Interface\\Addons\\SharedMedia\\statusbar\\Gloss")
+	SML:Register("statusbar", "Otravi",		"Interface\\Addons\\SharedMedia\\statusbar\\Otravi")
+	SML:Register("statusbar", "Perl",		"Interface\\Addons\\SharedMedia\\statusbar\\Perl")
+	SML:Register("statusbar", "Round",		"Interface\\Addons\\SharedMedia\\statusbar\\Round")
+	SML:Register("statusbar", "Smooth",		"Interface\\Addons\\SharedMedia\\statusbar\\Smooth")
+	SML:Register("statusbar", "Steel",		"Interface\\Addons\\SharedMedia\\statusbar\\Steel")
+
+end
