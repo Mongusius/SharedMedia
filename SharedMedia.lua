8e2ade7262bc2a92bbff75c1c4c6b3a5a5e16fe5
elkano <elkano@1a92d559-679e-4034-997b-ece41c1f7aad>
2007-05-13 11:55:02 +0000

    SharedMedia: cleaned up latest commits
    git-svn-id: https://repos.wowace.com/wow/sharedmedia/tmp/trunk/SharedMedia@59 1a92d559-679e-4034-997b-ece41c1f7aad

--- SharedMedia.lua
+++ SharedMedia.lua
@@ -1,3 +1,6 @@
+-- tab size is 4
+-- registrations for media from the client itself belongs into SharedMediaLib
+
 local SML = AceLibrary("SharedMedia-1.0")
 
 SharedMedia = AceLibrary("AceAddon-2.0"):new()
@@ -7,64 +10,61 @@ SharedMedia.revision = tonumber(string.sub("$Revision$", 12, -3)) or 1
 function SharedMedia:OnInitialize()
 
 -- -----
---   FONT
+-- BACKGROUND
 -- -----
-	SML:Register("font", "BigNoodleTilting",	"Interface\\Addons\\SharedMedia\\fonts\\BigNoodleTilting.ttf")
-	SML:Register("font", "BlackChancery",		"Interface\\Addons\\SharedMedia\\fonts\\BlackChancery.ttf")
-	SML:Register("font", "Emblem",			"Interface\\Addons\\SharedMedia\\fonts\\Emblem.ttf")
-
--- -----
---   STATUSBAR
--- -----
-	SML:Register("statusbar", "Aluminium",		"Interface\\Addons\\SharedMedia\\statusbar\\Aluminium")
-	SML:Register("statusbar", "BantoBar",		"Interface\\Addons\\SharedMedia\\statusbar\\BantoBar")
-	SML:Register("statusbar", "Bars",		"Interface\\Addons\\SharedMedia\\statusbar\\Bars")
-	SML:Register("statusbar", "Bumps",		"Interface\\Addons\\SharedMedia\\statusbar\\Bumps")
-	SML:Register("statusbar", "Button",		"Interface\\Addons\\SharedMedia\\statusbar\\Button")
-	SML:Register("statusbar", "Charcoal",		"Interface\\Addons\\SharedMedia\\statusbar\\Charcoal")
-	SML:Register("statusbar", "Cilo",		"Interface\\Addons\\SharedMedia\\statusbar\\Cilo")
-	SML:Register("statusbar", "Cloud",		"Interface\\Addons\\SharedMedia\\statusbar\\Cloud")
-	SML:Register("statusbar", "Comet",		"Interface\\Addons\\SharedMedia\\statusbar\\Comet")
-	SML:Register("statusbar", "Dabs",		"Interface\\Addons\\SharedMedia\\statusbar\\Dabs")
-	SML:Register("statusbar", "DarkBottom",		"Interface\\Addons\\SharedMedia\\statusbar\\DarkBottom")
-	SML:Register("statusbar", "Diagonal",		"Interface\\Addons\\SharedMedia\\statusbar\\Diagonal")
-	SML:Register("statusbar", "Falumn",		"Interface\\Addons\\SharedMedia\\statusbar\\Falumn")
-	SML:Register("statusbar", "Fifths",		"Interface\\Addons\\SharedMedia\\statusbar\\Fifths")
-	SML:Register("statusbar", "Flat",		"Interface\\Addons\\SharedMedia\\statusbar\\Flat")
-	SML:Register("statusbar", "Fourths",		"Interface\\Addons\\SharedMedia\\statusbar\\Fourths")
-	SML:Register("statusbar", "Glaze",		"Interface\\Addons\\SharedMedia\\statusbar\\Glaze")
-	SML:Register("statusbar", "Gloss",		"Interface\\Addons\\SharedMedia\\statusbar\\Gloss")
-	SML:Register("statusbar", "Grid",		"Interface\\Addons\\SharedMedia\\statusbar\\Grid")
-	SML:Register("statusbar", "Hatched",		"Interface\\Addons\\SharedMedia\\statusbar\\Hatched")
-	SML:Register("statusbar", "LiteStep",		"Interface\\Addons\\SharedMedia\\statusbar\\LiteStep")
-	SML:Register("statusbar", "Minimalist",		"Interface\\Addons\\SharedMedia\\statusbar\\Minimalist")
-	SML:Register("statusbar", "Otravi",		"Interface\\Addons\\SharedMedia\\statusbar\\Otravi")
-	SML:Register("statusbar", "Outline",		"Interface\\Addons\\SharedMedia\\statusbar\\Outline")
-	SML:Register("statusbar", "Perl",		"Interface\\Addons\\SharedMedia\\statusbar\\Perl")
-	SML:Register("statusbar", "Perl v2",		"Interface\\Addons\\SharedMedia\\statusbar\\Perl2")
-	SML:Register("statusbar", "Rain",		"Interface\\Addons\\SharedMedia\\statusbar\\Rain")
-	SML:Register("statusbar", "Round",		"Interface\\Addons\\SharedMedia\\statusbar\\Round")
-	SML:Register("statusbar", "Skewed",		"Interface\\Addons\\SharedMedia\\statusbar\\Skewed")
-	SML:Register("statusbar", "Smooth",		"Interface\\Addons\\SharedMedia\\statusbar\\Smooth")
-	SML:Register("statusbar", "Smooth v2",		"Interface\\Addons\\SharedMedia\\statusbar\\Smoothv2")
-	SML:Register("statusbar", "Smudge",		"Interface\\Addons\\SharedMedia\\statusbar\\Smudge")
-	SML:Register("statusbar", "Steel",		"Interface\\Addons\\SharedMedia\\statusbar\\Steel")
-	SML:Register("statusbar", "Water",		"Interface\\Addons\\SharedMedia\\statusbar\\Water")
-	SML:Register("statusbar", "Wisps",		"Interface\\Addons\\SharedMedia\\statusbar\\Wisps")
 
 -- -----
 --  BORDER
 -- ----
-	SML:Register("border", "Blizzard Tooltip", "Interface\\Tooltips\\UI-Tooltip-Border")
-	SML:Register("border", "Blizzard Dialog", "Interface\\DialogFrame\\UI-DialogBox-Border")
 
 -- -----
--- BACKGROUNDS
+--   FONT
 -- -----
-	SML:Register("background", "Blizzard Out of Control", "Interface\\FullScreenTextures\\OutOfControl")
-	SML:Register("background", "Blizzard Low Health", "Interface\\FullScreenTextures\\LowHealth")
-	SML:Register("background", "Blizzard Tabard Background", "Interface\\TabardFrame\\TabardFrameBackground")
-	
-	
+	SML:Register("font", "BigNoodleTilting",		"Interface\\Addons\\SharedMedia\\fonts\\BigNoodleTilting.ttf")
+	SML:Register("font", "BlackChancery",			"Interface\\Addons\\SharedMedia\\fonts\\BlackChancery.ttf")
+	SML:Register("font", "Emblem",					"Interface\\Addons\\SharedMedia\\fonts\\Emblem.ttf")
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+	SML:Register("statusbar", "Aluminium",			"Interface\\Addons\\SharedMedia\\statusbar\\Aluminium")
+	SML:Register("statusbar", "BantoBar",			"Interface\\Addons\\SharedMedia\\statusbar\\BantoBar")
+	SML:Register("statusbar", "Bars",				"Interface\\Addons\\SharedMedia\\statusbar\\Bars")
+	SML:Register("statusbar", "Bumps",				"Interface\\Addons\\SharedMedia\\statusbar\\Bumps")
+	SML:Register("statusbar", "Button",				"Interface\\Addons\\SharedMedia\\statusbar\\Button")
+	SML:Register("statusbar", "Charcoal",			"Interface\\Addons\\SharedMedia\\statusbar\\Charcoal")
+	SML:Register("statusbar", "Cilo",				"Interface\\Addons\\SharedMedia\\statusbar\\Cilo")
+	SML:Register("statusbar", "Cloud",				"Interface\\Addons\\SharedMedia\\statusbar\\Cloud")
+	SML:Register("statusbar", "Comet",				"Interface\\Addons\\SharedMedia\\statusbar\\Comet")
+	SML:Register("statusbar", "Dabs",				"Interface\\Addons\\SharedMedia\\statusbar\\Dabs")
+	SML:Register("statusbar", "DarkBottom",			"Interface\\Addons\\SharedMedia\\statusbar\\DarkBottom")
+	SML:Register("statusbar", "Diagonal",			"Interface\\Addons\\SharedMedia\\statusbar\\Diagonal")
+	SML:Register("statusbar", "Falumn",				"Interface\\Addons\\SharedMedia\\statusbar\\Falumn")
+	SML:Register("statusbar", "Fifths",				"Interface\\Addons\\SharedMedia\\statusbar\\Fifths")
+	SML:Register("statusbar", "Flat",				"Interface\\Addons\\SharedMedia\\statusbar\\Flat")
+	SML:Register("statusbar", "Fourths",			"Interface\\Addons\\SharedMedia\\statusbar\\Fourths")
+	SML:Register("statusbar", "Glaze",				"Interface\\Addons\\SharedMedia\\statusbar\\Glaze")
+	SML:Register("statusbar", "Gloss",				"Interface\\Addons\\SharedMedia\\statusbar\\Gloss")
+	SML:Register("statusbar", "Grid",				"Interface\\Addons\\SharedMedia\\statusbar\\Grid")
+	SML:Register("statusbar", "Hatched",			"Interface\\Addons\\SharedMedia\\statusbar\\Hatched")
+	SML:Register("statusbar", "LiteStep",			"Interface\\Addons\\SharedMedia\\statusbar\\LiteStep")
+	SML:Register("statusbar", "Minimalist",			"Interface\\Addons\\SharedMedia\\statusbar\\Minimalist")
+	SML:Register("statusbar", "Otravi",				"Interface\\Addons\\SharedMedia\\statusbar\\Otravi")
+	SML:Register("statusbar", "Outline",			"Interface\\Addons\\SharedMedia\\statusbar\\Outline")
+	SML:Register("statusbar", "Perl",				"Interface\\Addons\\SharedMedia\\statusbar\\Perl")
+	SML:Register("statusbar", "Perl v2",			"Interface\\Addons\\SharedMedia\\statusbar\\Perl2")
+	SML:Register("statusbar", "Rain",				"Interface\\Addons\\SharedMedia\\statusbar\\Rain")
+	SML:Register("statusbar", "Round",				"Interface\\Addons\\SharedMedia\\statusbar\\Round")
+	SML:Register("statusbar", "Skewed",				"Interface\\Addons\\SharedMedia\\statusbar\\Skewed")
+	SML:Register("statusbar", "Smooth",				"Interface\\Addons\\SharedMedia\\statusbar\\Smooth")
+	SML:Register("statusbar", "Smooth v2",			"Interface\\Addons\\SharedMedia\\statusbar\\Smoothv2")
+	SML:Register("statusbar", "Smudge",				"Interface\\Addons\\SharedMedia\\statusbar\\Smudge")
+	SML:Register("statusbar", "Steel",				"Interface\\Addons\\SharedMedia\\statusbar\\Steel")
+	SML:Register("statusbar", "Water",				"Interface\\Addons\\SharedMedia\\statusbar\\Water")
+	SML:Register("statusbar", "Wisps",				"Interface\\Addons\\SharedMedia\\statusbar\\Wisps")
 
 end
